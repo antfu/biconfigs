a7755bd6d7448e3c741480ebcd19756de36b06cc
Anthony Fu <anthonyfu117@hotmail.com>
2016-09-28 05:56:09 +0800

    Add memory test

---
+++ tests/test_memory.py
@@ -0,0 +1,27 @@
+import biconfigs
+import json
+
+class TestMemoryStorage(object):
+
+    def setup_class(cls):
+        cls.change_count = 0
+        def onchanged(config):
+            cls.change_count += 1
+
+        cls.config = biconfigs.BiConfigs(
+            path='::memory::',
+            default_value={'default':'value'},
+            onchanged=onchanged
+        )
+        assert cls.change_count == 0
+        assert json.dumps(cls.config) == '{"default": "value"}'
+
+    def test_basic_update(self):
+        self.config.clear()
+        assert json.dumps(self.config) == '{}'
+
+        self.config['item1'] = 'value'
+        assert json.dumps(self.config) == '{"item1": "value"}'
+
+        del(self.config['item1'])
+        assert json.dumps(self.config) == '{}'
